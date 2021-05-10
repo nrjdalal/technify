2e26e4a9d5bf603b1b1c66b9aaa3c8b3ec16e687
Neeraj Dalal <nd941z@gmail.com>
2021-05-10 16:21:40 +0530

    UPDATE

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'import/extensions': 'off',
+    'no-console': 'off',
+    semi: ['error', 'never'],
+  },
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+Run the following commands -
+
+```sh
+npm i -g typescript
+npm i -D typescript
+npm i -D ts-node @types/node
+
+npm i -g eslint
+npm i -D eslint
+npm i -D @typescript-eslint/eslint-plugin @typescript-eslint/parser
+npm i -D eslint-config-airbnb-base eslint-plugin-import
+
+npm i -D prettier
+npm i -D eslint-config-prettier eslint-plugin-prettier
+```
